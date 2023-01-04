7b6d1f49cc5cfb308814e64ee09f37a5f7c6d8d3
Matthew Golliday <matthewgolliday4@gmail.com>
2023-01-03 19:30:17 -0600

    Ops11

--- Ops11.ps1.py
+++ Ops11.ps1.py
@@ -2,11 +2,11 @@ Ops11.ps1
 #Task 1
 Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
 #Task 2
-netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
+netsh advfirewall firewall add rule name= "Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
 #Task 3
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /vfDenyTSConnections /t REG_DWORD /d0 /f 
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f 
 #Task 4
-iex ((New=Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+iex ((New-Object System.Net.WebClient)).DownloadString('https://git.io/debloat')
 #Task 5
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft- Hyper-V -All 
 #Task 6
